4fbeabcc8c5b1c6d62803c432bf98ba54c9a9d43
user <dev@antalogic.com>
2020-08-03 23:16:39 +0300

    Updates

---
+++ precache-manifest.8c007a6f38cc65a451371df6f9a633c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9744daa6a149fa0de5cddcb1d4976dd7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c97761c5072dbc1e6efd",
+    "url": "/monsters-rolodex/static/css/main.a7489e1b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c97761c5072dbc1e6efd",
+    "url": "/monsters-rolodex/static/js/main.39a4867b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
