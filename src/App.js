1b3832546737d77f3aea71c1ee16c480e2ec1c5a
user <dev@antalogic.com>
2020-08-03 22:41:33 +0300

    SearchBox added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
  
@@ -8,7 +9,8 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			monsters: []
+			monsters: [],
+			searchField: ''
 		}
 	}
 
@@ -21,9 +23,15 @@ class App extends Component {
 	}
 
 	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
 		return (
 			<div className="App">
-				<CardList monsters={this.state.monsters}/>
+				<SearchBox 
+					placeholder={"search monsters"} 
+					handleChange={(e) => this.setState({searchField: e.target.value})} 
+				/>
+				<CardList monsters={filteredMonsters}/>
 			</div>
 		);
 	}
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const SearchBox = ({ placeholder, }) => (
+    <input 
+        className="search"
+        type="search"
+        placeholder={placeholder}
+        onChange={(e) => this.setState({searchField: e.target.value})}
+    />
+);
